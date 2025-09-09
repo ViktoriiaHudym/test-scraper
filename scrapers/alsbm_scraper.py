d02ccdf0fbda7f09e22a809e59a0a8ddf2a32647
Viktoriia <vikahudym222@gmail.com>
2025-09-09 09:13:21 +0300

    feat: add some comments and docstring for _set_session_cookies()

--- scrapers/alsbm_scraper.py
+++ scrapers/alsbm_scraper.py
@@ -20,6 +20,7 @@ class AlsbmScraper(BaseScraper):
     def scrape_items(self, page_url: str):
         results = []
 
+        # Handles the PHPSESSID and sets the correct User-Agent and other headers for a POST request
         self._set_session_cookies()
         self._set_session_headers(self.config.POST_HEADERS)
 
@@ -27,6 +28,7 @@ class AlsbmScraper(BaseScraper):
             response = self.session.post(page_url, data=self.config.PAYLOAD, timeout=15)
             response.raise_for_status()
 
+            # Get all tables from page as a list of DataFrame objects
             page_tables = self.get_tables_from_html(response.text)
 
             if page_tables:
@@ -44,12 +46,22 @@ class AlsbmScraper(BaseScraper):
 
 
     def _set_session_cookies(self):
+        """
+        Updates session cookies with a valid PHPSESSID.
+
+        Reuses cookies from previous run if available.
+        If PHPSESSID missing or expired, fetches a fresh one from self.base_url.
+        Saves cookies back to file for future runs.
+        """
+
+        # here we will store cookies
         os.makedirs("cookies", exist_ok=True)
         cookies_file = f'cookies/{self.__class__.__name__}_cookies.pkl'
 
+        # set the GET headers, which are needed for the initial request to collect cookies
         self._set_session_headers(self.config.GET_HEADERS)
 
-        # Try to load cookies from the file created in a previous run
+        # try to load cookies from the file created in a previous run
         if os.path.exists(cookies_file):
             try:
                 with open(cookies_file, "rb") as f:
@@ -60,13 +72,16 @@ class AlsbmScraper(BaseScraper):
                 self.logger.warning(f"Could not load cookies from file: {e}")
 
         if "PHPSESSID" not in self.session.cookies.get_dict():
+            # Make a request to a page that make request in new session and sets a PHPSESSID cookie
             resp = self.session.get('https://alsbm.org/midwives')
             resp.raise_for_status()
 
         if "PHPSESSID" not in self.session.cookies.get_dict():
             self.logger.warning("Could not obtain PHPSESSID cookies from server")
         else:
-            self.logger.info("PHPSESSID cookies loaded from server successfully")
+            self.logger.info("PHPSESSID cookies successfully loaded from server")
+
+            # Save the current session cookies to a file for reuse in the next run
             with open(cookies_file, "wb") as f:
                 pickle.dump(self.session.cookies, f)
 
